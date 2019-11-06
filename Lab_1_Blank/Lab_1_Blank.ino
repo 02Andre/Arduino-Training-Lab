de939c36f5cff96fbb851ea5a0a2b2c3f6fb62ea
Andre Smith <02andre.smith@gulllakecs.org>
2019-11-06 12:57:01 -0500

    Add Blink

---
+++ Lab_1_Blank/Lab_1_Blank.ino
@@ -0,0 +1,23 @@
+/*Programmer: Andre 
+ * Date: 11.6.19
+ * Program: Blink
+ * 
+ * This program will turn on an LED for a specified amount of time, turn it
+ * off, and back on in a while loop
+ */
+
+// initialize our LED to pin 13
+int led = 13;
+
+void setup() {
+  // Using the Function pinMode to call the variable led and provide Output:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
