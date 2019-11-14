1a9de1d47228a5365abdc56077b7600426314c78
Andre Smith <02andre.smith@gulllakecs.org>
2019-11-14 14:03:28 -0500

    Intitial DigitalRead()- the Serial Port

---
+++ Lab_1_Blank/Lab_1_Blank.ino
@@ -0,0 +1,17 @@
+/*Programmer: Andre 
+ * Date: 11.14.19
+ * Program: digitalRead() - the Serial Port
+ * 
+ * Reads a Digital input on pin2, prints the result to the serial monitor
+ */
+
+
+void setup() {
+  
+  
+}
+
+void loop() {
+  
+
+}
